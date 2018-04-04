3f721ba3a7d97bbff809ee8276d7a4491dc4a05b
Tiffany Wu <tiffwu88@gmail.com>
2018-04-04 11:10:24 -0400

    Fixed Error when can not find user

--- my-app/src/FetchData.js
+++ my-app/src/FetchData.js
@@ -6,61 +6,69 @@ import TextField from 'material-ui/TextField';
 import Table, {TableBody, TableCell, TableHead, TableRow} from 'material-ui/Table';
 
 var styles = {
-    "dataStyle": {
-        "marginTop": "20px",
-        "marginBottom": "20px",
-        "color": "blue",
+  "dataStyle": {
+    "marginTop": "20px",
+    "marginBottom": "20px",
+    "color": "blue",
     }
-}
+};
 
 class FetchData extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            "nameTextField": "",  //contents in text box
-            "HRdata": ["Nothing Yet"],
-            "HRtime": []
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      "nameTextField": "",  //contents in text box
+      "HRdata": [],
+      "HRtime": [],
+    };
+  }
 
-    onNameTextFieldChange = (event) => {
-        // Update the nameTextField state whenever the text field is changed or perturbed in any way:
-        this.setState({"nameTextField": event.target.value});
-    }
+  onNameTextFieldChange = (event) => {
+    // Update the nameTextField state whenever the text field is changed or perturbed in any way:
+    this.setState({"nameTextField": event.target.value});
+  };
 
-    onButtonClick = (event) => {
-        console.log(this.state.nameTextField); // log the current nameTextField content
-    }
+  onButtonClick = (event) => {
+    console.log(this.state.nameTextField); // log the current nameTextField content
+  };
 
-    getData = () => {
-        var user_email = this.state.nameTextField
-        axios.get("http://vcm-3574.vm.duke.edu:5000/api/heart_rate/"+user_email).then( (response) => {
+  getData = () => {
+    var user_email = this.state.nameTextField;
+    axios.get("http://vcm-3574.vm.duke.edu:5000/api/heart_rate/"+user_email).then( (response) => {
 
-            console.log(response);
-            //console.log(response.status);
-            this.setState({"HRdata": response.data.heart_rate, "HRtime":response.data.heart_rate_times});
-        })
-    }
+      console.log(response);
+      //console.log(response.status);
+      if (response.data == "Unknown User"){
+        this.setState({"HRdata": ["Unknown User"], "HRtime":[]})
+      }
+      else {
+        this.setState({"HRdata": response.data.heart_rate, "HRtime": response.data.heart_rate_times});
+      }
+      })
+  };
 
-    render() {
-        return (
-            <div>
-                <TextField onChange={this.onNameTextFieldChange}/>
-                <Button variant="raised" onClick={this.getData}>
-                    Get Data
-                </Button>
+  render() {
+    return (
+      <div>
+        <TextField onChange={this.onNameTextFieldChange}/>
+        <Button variant="raised" onClick={this.getData}>
+          Get Data
+        </Button>
 
-              <div style={styles.dataStyle}>
-                {JSON.stringify(this.state.HRdata)}
-              </div>
+        <p>Heart Rate data: </p>
+        <div style={styles.dataStyle}>
+          {JSON.stringify(this.state.HRdata)}
+        </div>
 
-              <div style={styles.dataStyle}>
-                {JSON.stringify(this.state.HRtime)}
-              </div>
+        <p> Associated times: </p>
+        <div style={styles.dataStyle}>
+          {JSON.stringify(this.state.HRtime)}
+        </div>
 
-            </div>
-        )
-    }
+      </div>
+
+    )
+  }
 }
 
 export default FetchData;
